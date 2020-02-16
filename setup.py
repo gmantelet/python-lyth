0da771f2ac64b642376cfe7d019b8c0942c61fb8
Guillaume Mantelet <gmantele-3o@outlook.com>
2020-02-16 04:40:08 -0600

    Cleaning working directory before bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='lyth',
-    version='0.0.1',
+    version='0.0.2',
     license='BSD-2-Clause',
     description='A (monolithic) compiled language',
     long_description='%s\n%s' % (
