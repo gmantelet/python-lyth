d78ed1624dada3e4a81a09913b8e33245e1304da
Guillaume Mantelet <gmantele-3o@outlook.com>
2020-02-19 22:25:34 -0600

    Decreased cognitive burden on lexer

--- src/lyth/compiler/lexer.py
+++ src/lyth/compiler/lexer.py
@@ -52,71 +52,40 @@ class Lexer:
         try:
             char = next(self.scan)
 
-            #
-            # 1. Spaces are token delimitors.
-            #
             if char.isspace():
+                return self.handle_space_as_delimiter()
 
-                #
-                # 1.1. First space encountered after an existing token is being
-                #      constructed (aggregated). This causes the current token
-                #      to be returned.
-                #
-                if self.current_token is not None:
-                    token = self.current_token
-                    self.current_token = None
-                    return token()
-
-                #
-                # 1.2. Multiple spaces encountered. Skip, and move to next
-                #      character
-                #
-                else:
-                    return next(self)
-
-            #
-            # 2. First character of a new token. Create a new token and move to
-            #    next character.
-            #
-            elif self.current_token is None:
-                self.current_token = Token(char, self.scan)
-                return next(self)
-
-            #
-            # 3. Successive character of an existing token. Go on building
-            #    current token (aggregate) and get next character from scanner.
-            #
-            #    NOTE: This step may raise MISSING_SPACE_AFTER_OPERATOR error.
-            #          The lexer needs to check if it can ignore the error. For
-            #          example, '+5' or '-5' are valid cases, and the parser
-            #          needs to check if it is a sign or an operator.
-            #
-            else:  # Meaning, self.current_token is not None
-                self.current_token += char
-                return next(self)
+            else:
+                return self.handle_char(char)
 
         except StopIteration:
             return self.handle_eof()
 
-        #
-        # 6. We tried to concatenate a token, but there was a LythSyntaxError raised.
-        #
         except LythSyntaxError as lyth_error:
-            token = self.current_token
+            return self.handle_syntax_error(char, lyth_error)
 
-            #
-            # 6.1. The lexical analysis tolerates missing space after '+' or
-            #      '-' only if these symbols represent a sign, and not an
-            #      operator. The Parser would raise such an error at its stage.
-            if lyth_error.msg is LythError.MISSING_SPACE_AFTER_OPERATOR:
-                if token is not None and token.symbol in (Symbol.ADD, Symbol.SUB):
-                    self.current_token = Token(char, self.scan)
-                    return token()
+    def handle_char(self, char):
+        """
+        Handle the non space character obtained from the Scanner.
 
-            #
-            # 6.2. In any other LythSyntaxError, the exception is propagated.
-            #
-            raise
+        Two options depends on whether we are currently building up a token or
+        we need to create a new one:
+        1. First character of a new token. Create a new token and move to next
+           character.
+        2. Successive character of an existing token. Go on building current
+           token (aggregate) and get next character from scanner.
+                NOTE: This step may raise MISSING_SPACE_AFTER_OPERATOR error.
+                      The lexer needs to check if it can ignore the error. For
+                      example, '+5' or '-5' are valid cases, and the parser
+                      needs to check if it is a sign or an operator.
+        """
+        if self.current_token is None:
+            self.current_token = Token(char, self.scan)
+
+        else:
+            self.current_token += char
+
+        return next(self)
 
     def handle_eof(self):
         """
@@ -148,3 +117,40 @@ class Lexer:
             token = self.current_token
             self.current_token = None
             return token()
+
+    def handle_space_as_delimiter(self):
+        """
+        Spaces are token delimitors.
+
+        There are two options depending on whether it is the first time the
+        scanner encounters a space:
+        1. First space encountered after an existing token is being constructed
+           (aggregated). This causes the current token to be returned.
+        2. Multiple spaces encountered. Skip, and move to next character
+        """
+        if self.current_token is not None:
+            token = self.current_token
+            self.current_token = None
+            return token()
+
+        else:
+            return next(self)
+
+    def handle_syntax_error(self, char, error):
+        """
+        We tried to concatenate a token, but there was a LythSyntaxError raised.
+
+        There are a couple of tolerated cases at this stage of the analysis:
+        1. The lexical analysis tolerates missing space after '+' or '-' only
+           if these symbols represent a sign, and not an operator. The Parser
+            would raise such an error at its stage.
+        2. In any other LythSyntaxError, the exception is propagated.
+        """
+        token = self.current_token
+
+        if error.msg is LythError.MISSING_SPACE_AFTER_OPERATOR:
+            if token is not None and token.symbol in (Symbol.ADD, Symbol.SUB):
+                self.current_token = Token(char, self.scan)
+                return token()
+
+        raise
